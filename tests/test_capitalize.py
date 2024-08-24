c12cce9ce25c65bdb6ba9fa324e6606566a09e32
DolAndd <dolganov2002@mail.ru>
2024-08-24 18:00:01 +0000

    update test

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,7 +1,7 @@
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
+assert capitalize('hello') == 'Hello'
+    
 
 if capitalize('') != '':
     raise Exception('Функция работает неверно!')
